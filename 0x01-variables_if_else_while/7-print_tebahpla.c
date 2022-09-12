0a9debf13bca2ac18839a81c392f0958798e5f65
Safegal <110991249+Safegal@users.noreply.github.com>
2022-09-12 22:14:31 +0300

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,11 +10,14 @@
 
 int main(void)
 {
-	char low;
+char low;
 
-	for (low = 'z'; low >= 'a'; low--)
-		putchar(low);
-		putchar('\n');
+for (low = 'z'; low >= 'a'; low--)
+{
+   putchar(low);
+		
 
-	return (0);
+}
+putchar('\n');	
+return (0);
 }
