160d2430265a312f8e73ebd1464d6748659e8351
Safegal <110991249+Safegal@users.noreply.github.com>
2022-09-14 22:09:53 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,6 +6,13 @@
  * Description: function returns 1 if the character is a
  * letter, lowercase or uppercase.
  * Return: 1 or 0 in otherwise.
+ *int _isalpha(int c)
+ *{
+ *	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+ *		return (1);
+ *	else
+ *		return (0);
+ *}
  */
 int _isalpha(int c)
 {
@@ -21,4 +28,5 @@ int _isalpha(int c)
 		}
 	}
 	return (isletter);
+	
 }
