304f546cd6094f851ba09905117c9a3e5e7e9d0c
Safegal <110991249+Safegal@users.noreply.github.com>
2022-09-15 21:50:42 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i = 1;
 
-	for (; i < 100 ; i++)
+	for (; i <= 100 ; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
@@ -20,7 +20,6 @@ int main(void)
 		else
 			printf("%d ", i);
 	}
-	printf("Buzz\n");
-
+	
 	return (0);
 }
