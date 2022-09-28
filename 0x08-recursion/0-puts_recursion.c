76555a6e79010aa04b5d78265a907a862a187c6b
Safegal <110991249+Safegal@users.noreply.github.com>
2022-09-28 19:59:04 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _puts_recursion - a function that prints a string, followed by a new line.
@@ -7,11 +7,6 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-		_putchar('\n');
+    puts(s);
+  return(0);
 }
